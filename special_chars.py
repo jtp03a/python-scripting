c2452db0fda3c6e35c9ab02f54d1acf2c7030e44
Jake Peterson <a3sir86@gmail.com>
2022-11-18 08:47:28 -0600

    first commit

---
+++ special_chars.py
@@ -0,0 +1,17 @@
+# Special Chars
+# \n - New Line
+# \b - Backspace
+# \t - Tab
+# \ escape
+# Double Quotes "''"
+# Slashes - Windows Path - \\
+
+print("Hello \b\bWorld")
+print("Text on \nnew line")
+print("\t Add a tab")
+print("Jake\'s python")
+print('A "Python" Script')
+# or
+print("A \"Python\" Script")
+print("C:\\Users\\Folder")
+print("~/linux/path")
