39a7f0f1efae0c7f12e6547659d2594060912bcb
Jake Peterson <a3sir86@gmail.com>
2022-11-22 09:53:31 -0600

    added more os, and os walk

--- find_file.py
+++ find_file.py
@@ -1,14 +1,19 @@
 import os
+import string
 
 # platform independent system wide search for a file using os walk
 # Linux find - find / -iname host.conf
 
 req_file = input("Enter the file name to find: ")
 
-# works for linux but not windows
+# works for linux because / is the root but there is not equivalent in windows
 # path = "/"
-
-path = "C:\\Users\\xorci\\Documents"
+# need a way to identify all drives in Windows
+# i.e. path = "C:\\Users\\xorci\\Documents"
+pos_drive_names = string.ascii_letters
+for drive_ltr in pos_drive_names:
+  if os.path.exists(f'{drive_ltr}:\\')
+    print(f'{drive_ltr}:\\')
 
 for p, d, f in list(os.walk(path)):
   print(p, d, f)
